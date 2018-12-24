b2d9790b5d3336bd54c0b4c81c6d91318f1bfa33
Alexander Stanev <xelaxStanev@gmail.com>
2018-12-24 16:14:02 +0200

    Added ContextFactory and Migrations.

---
+++ src/Data/GameDatabase.Data/GameDatabaseContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using GamesDatabase.Data;
+
+namespace GameDatabase.Data
+{
+    public class GameDatabaseContextFactory : IDesignTimeDbContextFactory<GamesDatabaseContext>
+    {
+        public GamesDatabaseContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<GamesDatabaseContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            // Stop client query evaluation
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new GamesDatabaseContext(builder.Options);
+        }
+    }
+}
--- src/Data/GameDatabase.Data/Migrations/20181224112642_InitialCreate.Designer.cs
+++ src/Data/GameDatabase.Data/Migrations/20181224112642_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using GamesDatabase.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GamesDatabase.Data.Migrations
 {
     [DbContext(typeof(GamesDatabaseContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181224112642_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
